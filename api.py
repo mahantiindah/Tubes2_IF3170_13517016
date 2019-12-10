4032f8fa85805a14567d5de3ab8017fbed1f6695
unknown <Ihsan Imaduddin Azhar>
2019-12-10 20:22:02 +0900

    done inference engine

--- api.py
+++ api.py
@@ -1,13 +1,35 @@
 import cv2
 import numpy as np
+import math
 
 from clips import Environment, Symbol
 
 
 
 def getAngle(a, b, c):
-    ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
+    ax = a[0][0]
+    ay = a[0][1]
+    bx = b[0][0]
+    by = b[0][1]
+    cx = c[0][0]
+    cy = c[0][1]
+
+    ang = math.degrees(math.atan2(cx-bx, cy-by) - math.atan2(ax-bx, ay-by))
     return ang + 360 if ang < 0 else ang
+
+def getSisi(titik):
+    sisi = []
+    for i in range(len(titik)):
+        sisi.append(getDistance(titik[i],titik[(i+1)%len(titik)]))
+
+    return sisi
+
+def getSudut(titik):
+    sudut = []
+    for i in range(len(titik)):
+        sudut.append(getAngle(titik[i],titik[(i+1)%len(titik)],titik[(i+2)%len(titik)]))
+
+    return sudut
 
 def isSamaSisi(app):
     edgeList = []
@@ -16,7 +38,13 @@ def isSamaSisi(app):
 
     return checkEqual(edgeList)
 
-        
+def compareGalat(a,b):
+    temp = abs(a - b)
+    if(temp < 3):
+        return True
+    else:
+        return False
+
 
 def checkEqual(lst):
    return lst[1:] == lst[:-1]
@@ -28,6 +56,115 @@ def getDistance(a,b):
     by = b[0][1]
 
     return np.sqrt((ax - bx)**2 + (ay - by)**2)
+
+def getAngleFact(ang):
+    jumlah = len(ang)
+    fact = []
+    if(jumlah == 3):
+        tumpul = any(sudut > 90 for sudut in ang) 
+        siku = any(compareGalat(sudut,90) for sudut in ang) 
+
+        if(tumpul):
+            return '(sudut tumpul)'
+        elif(siku):
+            return '(sudut siku)'
+        else:
+            return '(sudut lancip)'
+        
+    elif(jumlah == 4):
+        notTotal = any(not compareGalat(ang[0],angle) for angle in ang)
+
+        if(not notTotal):
+            return '(sudut total)'
+        else:
+            usedNumber = []
+            for x in ang:
+                count = ang.count(x)
+                countUsed = usedNumber.count(x)
+                if(count > 1 and countUsed == 0):
+                    countUsed.append(x)
+            
+            if(len(usedNumber) == 2):
+                if(not compareGalat(usedNumber[0],usedNumber[1])):
+                    return '(sudut kembar-dua)'
+            
+            if(len(usedNumber) == 1):
+                return '(sudut kembar-satu)'
+            
+            if(len(usedNumber) == 0):
+                return '(sudut berantakan)'
+
+        return fact
+
+def getEdgeFact(edges):
+    jumlah = len(edges)
+    notTotal = any(not compareGalat(edges[0],edge) for edge in edges)
+    fact = []
+    if(jumlah == 3):
+        if(not notTotal):
+            return '(sisi total)'
+        else:
+            usedNumber = []
+            for x in edges:
+                count = edges.count(x)
+                countUsed = usedNumber.count(x)
+                if(count > 1 and countUsed == 0):
+                    usedNumber.append(x)
+            
+            if(len(usedNumber) == 2):
+                if(not compareGalat(usedNumber[0],usedNumber[1])):
+                    return '(sisi kembar-dua)'
+            
+            if(len(usedNumber) == 1):
+                return '(sisi kembar-satu)'
+            
+            if(len(usedNumber) == 0):
+                return '(sisi berantakan)'
+
+        return fact
+
+    elif(jumlah == 4):
+
+        if(not notTotal):
+            return '(sisi total)'
+        else:
+            usedNumber = []
+            for x in edges:
+                count = edges.count(x)
+                countUsed = usedNumber.count(x)
+                if(count > 1 and countUsed == 0):
+                    usedNumber.append(x)
+            
+            if(len(usedNumber) == 2):
+                if(not compareGalat(usedNumber[0],usedNumber[1])):
+                    return '(sisi kembar-dua)'
+            
+            if(len(usedNumber) == 1):
+                return '(sisi kembar-satu)'
+            
+            if(len(usedNumber) == 0):
+                return '(sisi berantakan)'
+
+        return fact
+
+    elif(jumlah == 5):
+        if(not notTotal):
+            return '(sisi total)'
+        else:
+            return '(sisi berantakan)'
+
+        return fact
+
+        
+    elif(jumlah == 6):
+
+        if(not notTotal):
+            return '(sisi total)'
+        else:
+            return '(sisi berantakan)'
+
+        return fact
+
 
 e = Environment()
 
@@ -45,7 +182,8 @@ contours, _ = cv2.findContours(
 # Pilih font
 font = cv2.FONT_HERSHEY_COMPLEX
 
-for cnt in contours:
+
+for cnt in contours[1:]:
     # Aproksimasi sisi polygon
     approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
 
@@ -58,46 +196,52 @@ for cnt in contours:
     # print(cv2.arcLength(cnt, True))
 
 
-    
+    for a in approx:
+
+            print("(titik " + str(a[0][0]) + " " + str(a[0][1]) + ")")
+
+    for i in getSisi(approx):
+        print(i)
+
+    for i in getSudut(approx):
+        print(i)
+
+
+                
 
     # Klasifikasi berdasarkan jumlah sisi
 
+    print(len(approx))
     if len(approx) == 3:
-        # e.assert_string("(titik 1 1)")
-        # e.assert_string("(titik 1 2)")
-        # e.assert_string("(titik 2 1)")
-
         # Assert fakta berupa titik shape
-        '''for a in approx:
-
-            print("(titik " + str(a[0][0]) + " " + str(a[0][1]) + ")")
-            strInput = "(titik " + str(a[0][0]) + " " + str(a[0][1]) + ")"
-            print(strInput)
-            e.assert_string(strInput)'''
-        if(isSamaSisi(approx)):
-            e.assert_string('(sisi total)')
-        e.assert_string('(titik 3)')
         
+        e.assert_string(getEdgeFact(getSisi(approx)))
+        e.assert_string(getAngleFact(getSudut(approx)))
+        e.assert_string('(titik 3)')
             
         cv2.putText(img, "Segitiga", (x, y), font, 1, (0))
+   
     elif len(approx) == 4:
-        if(isSamaSisi(approx)):
-            e.assert_string('(sisi total)')
+
+        e.assert_string(getEdgeFact(getSisi(approx)))
+        e.assert_string(getAngleFact(getSudut(approx)))
         e.assert_string('(titik 4)')
+
+            
         cv2.putText(img, "Persegi", (x, y), font, 1, (0))
 
     elif len(approx) == 5:
-        if(isSamaSisi(approx)):
-            e.assert_string('(sisi total)')
+        e.assert_string(getEdgeFact(getSisi(approx)))
         e.assert_string('(titik 5)')
         cv2.putText(img, "Segi Lima", (x, y), font, 1, (0))
 
     elif len(approx) == 6:
 
-        if(isSamaSisi(approx)):
-            e.assert_string('(sisi total)')
+        e.assert_string(getEdgeFact(getSisi(approx)))
         e.assert_string('(titik 6)')
         cv2.putText(img, "Segi Enam", (x, y), font, 1, (0))
+
+  
 
 
 # Print initial facts
@@ -118,9 +262,6 @@ print("\n\nFinal Facts :")
 for fact in e.facts():
     print(fact)
 
-'''print("\nRules :")
-for rule in e.rules():
-    print(rule)'''
 
 cv2.imshow("shapes", img)
 cv2.imwrite("result.jpg", img)
